122ef54fdfe9914a223e95ba3926373244462706
Minh Khoi Phan <pmka42@gmail.com>
2025-05-25 10:39:36 -0400

    Two Sum O(n).

--- Two_Sum/Properties/AssemblyInfo.cs
+++ Two_Sum/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Name")]
+[assembly: AssemblyTitle("Two_Sum")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Name")]
+[assembly: AssemblyProduct("Two_Sum")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]

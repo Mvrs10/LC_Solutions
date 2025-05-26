4354abc4d103c1614ae460db70a389effb0302a0
Minh Khoi Phan <pmka42@gmail.com>
2025-05-26 17:41:34 -0400

    IsPalindrome?

---
+++ Palindrome/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0) return false;
+            string stringX = x.ToString();
+            char[] array = stringX.ToCharArray();
+            int begin = 0, end = array.Length - 1;
+            while (begin < end)
+            {
+                (array[begin], array[end]) = (array[end], array[begin]);
+                begin++;
+                end--;
+            }
+            string reverseStringX = new string(array);
+            return stringX == reverseStringX;
+        }
+    }
+}

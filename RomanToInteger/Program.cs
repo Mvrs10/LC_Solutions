af0bfbe45cca7660e04de33dbd0e76b084775ab6
Minh Khoi Phan <pmka42@gmail.com>
2025-05-26 21:02:13 -0400

    RomanToInteger

---
+++ RomanToInteger/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanToInteger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Romans");
+        }
+        public int RomanToInt(string roman)
+        {
+            Dictionary<char, int> map = new Dictionary<char, int>()
+            {
+                {'I',1 },
+                {'V',5 },
+                {'X',10 },
+                {'L',50 },
+                {'C',100 },
+                {'D',500 },
+                {'M',1000 },
+            };
+            char[] chars = roman.ToCharArray();
+            int result = 0, prev = 0;
+            for (int i = chars.Length - 1; i >= 0; i--)
+            {
+                int value = map[chars[i]];
+                if (map[chars[i]] > prev)
+                {
+                    result = result + value;
+                }
+                else result = result - value;
+                prev = result;
+            }
+            return result;
+        }
+    }
+}

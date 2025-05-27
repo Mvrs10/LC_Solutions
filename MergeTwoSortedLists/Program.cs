00d9f18ff0223536de660fa5ce343ee7a447e454
Minh Khoi Phan <pmka42@gmail.com>
2025-05-26 21:56:31 -0400

    .

---
+++ MergeTwoSortedLists/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeTwoSortedLists
+{
+    public class ListNode
+    {
+        public int value;
+        public ListNode next;
+        public ListNode(int value = 0, ListNode next = null)
+        {
+            this.value = value;
+            this.next = next;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merge them");
+        }
+
+        public ListNode MergeTwoList(ListNode l1, ListNode l2)
+        {
+            ListNode node = new ListNode();
+            ListNode head = node;
+            while (l1 != null && l2 != null)
+            {
+                if (l1.value <= l2.value)
+                {
+                    node.next = l1;
+                    l1 = l1.next;
+                }
+                else
+                {
+                    node.next = l2;
+                    l2 = l2.next;
+                }
+                node = node.next;
+            }
+            if (l1 == null) node.next = l2;
+            else node.next = l1;
+            return head;
+        }
+    }
+}
+

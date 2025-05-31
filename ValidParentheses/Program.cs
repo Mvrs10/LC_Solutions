d21e108743e49cd4eb124683e39284a606b4c35a
Minh Khoi Phan <pmka42@gmail.com>
2025-05-30 22:02:22 -0400

    Stack&Dictionary

---
+++ ValidParentheses/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidParentheses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static bool isValid(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+            stack.Push('.');
+            Dictionary<char, char> map = new Dictionary<char, char>()
+            {
+                {'(',')'},
+                {'{','}'},
+                {'[',']'}
+            };
+            foreach (char c in s)
+            {                
+                if (map.ContainsKey(c))
+                {
+                    stack.Push(map[c]);
+                }
+                else
+                {
+                    char top = stack.Pop();
+                    if (top != c) return false;
+                }
+            }
+            return stack.Count == 1;
+        }
+    }
+}

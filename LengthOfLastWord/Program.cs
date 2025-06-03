7014981759d602decf82e5ac9274b6fb2f72a71d
Minh Khoi Phan <pmka42@gmail.com>
2025-06-03 00:23:42 -0400

    .

--- LengthOfLastWord/Program.cs
+++ LengthOfLastWord/Program.cs
@@ -10,6 +10,23 @@ namespace LengthOfLastWord
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Any last words");
+            int result = LengthOfLastWord("A B C d    ");
+            Console.WriteLine(result);
+        }
+
+        static int LengthOfLastWord(string s)
+        {
+            int count = 0;
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (s[i] != ' ')
+                {
+                    count++;
+                }
+                else if (count != 0) return count;
+            }
+            return count;
         }
     }
 }
--- SearchTarget/Program.cs
+++ SearchTarget/Program.cs
@@ -11,26 +11,21 @@ namespace SearchTarget
         static void Main(string[] args)
         {
             Console.WriteLine("Binary Search in disguise");
-            int result = SearchInsert(new int[] { 1, 3, 4, 9, 12, 25, 66, 198 }, 15);
-            Console.WriteLine(result);
+            int result = SearchInsert(new int[] { 1, 3, 4, 9, 12, 25, 66, 198 }, 4);
+            Console.WriteLine();
         }
 
         static int SearchInsert(int[] nums, int target)
         {
-            int low = 0, high = nums.Length - 1, index = 0;
+            int low = 0, high = nums.Length - 1;
             while (low <= high)
             {
                 int mid = (low + high) / 2;
-                index = mid;
-                if (nums[mid] == target)
-                {
-                    index = mid;
-                    return mid;
-                }
+                if (nums[mid] == target) return mid;
                 else if (nums[mid] > target) high = mid - 1;
                 else low = mid + 1;
             }
-            return index;
+            return low;
         }
     }
 }

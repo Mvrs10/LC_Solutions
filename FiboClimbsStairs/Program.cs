1928cbf4f889b2f02ab9bf3a1955dfd9029f7b9f
Minh Khoi Phan <pmka42@gmail.com>
2025-06-05 22:31:31 -0400

    Fibo climbs stairs.

---
+++ FiboClimbsStairs/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiboClimbsStairs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fibo climbs the stairs");
+            int result;
+            result = ClimbStairs(1);
+            result = ClimbStairs(2);
+            result = ClimbStairs(45);
+            Console.WriteLine(result);
+        }
+        private static int ClimbStairs(int n)
+        {
+            int waysForFirstStair = 1; int waysForSecondStair = 2;
+            int distinctWays = 0;
+            if (n<3)
+            {
+                return n;
+            }
+            while (n >= 3)
+            {
+                distinctWays = waysForFirstStair + waysForSecondStair;
+                waysForFirstStair = waysForSecondStair;
+                waysForSecondStair = distinctWays;
+                n--;
+            }
+            return distinctWays;
+        } 
+    }
+}

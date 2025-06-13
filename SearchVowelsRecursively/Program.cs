dc54371ff3d4c942bb133b9dfd1a2e6396ceebfb
Minh Khoi Phan <pmka42@gmail.com>
2025-06-12 22:48:48 -0400

    Search Vowels Recursively

---
+++ SearchVowelsRecursively/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchVowelsRecursively
+{
+    internal class Program
+    {
+        private static bool IsVowel(char c)
+        {
+            string vowels = "AaEeIiOoUu";
+            return vowels.Contains(c);
+        }
+
+        private static int SearchVowels(string str)
+        {
+            if (str.Length == 0) return 0;
+            char c = str[0];
+            int count = IsVowel(c) ? 1 : 0;
+            return count + SearchVowels(str.Substring(1));
+        }
+        static void Main(string[] args)
+        {
+            string str = " Hello World! ";
+            int count = SearchVowels(str);
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ SearchVowelsRecursively/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SearchVowelsRecursively")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SearchVowelsRecursively")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e1bbbbb4-b387-45a6-bddc-f9daa20e0eb5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

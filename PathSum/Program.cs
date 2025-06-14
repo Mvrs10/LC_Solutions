e3e17543a532c73b0bcae2c01f32ea3ebd6fcde2
Minh Khoi Phan <pmka42@gmail.com>
2025-06-14 14:17:53 -0400

    Path Sum.

---
+++ PathSum/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathSum
+{
+    internal class Program
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        private static bool Traverse(TreeNode root, int targetSum, int count)
+        {
+            if (root == null)
+            {
+                return false;
+            }
+            count = count + root.val;
+            if (root.left == null && root.right == null)
+            {
+                return count == targetSum;
+            }
+            return Traverse(root.left, targetSum, count) || Traverse(root.right, targetSum, count);
+        }
+
+        private static bool HasPathSum(TreeNode root, int targetSum)
+        {
+            if (root == null) return false;
+            return Traverse(root, targetSum, 0);
+        }
+
+        static void Main(string[] args)
+        {
+            TreeNode root = new TreeNode(5);
+
+            root.left = new TreeNode(4);
+            root.right = new TreeNode(8);
+
+            root.left.left = new TreeNode(11);
+
+            root.left.left.left = new TreeNode(7);
+            root.left.left.right = new TreeNode(2);
+
+            root.right.left = new TreeNode(13);
+            root.right.right = new TreeNode(4);
+
+            root.right.right.right = new TreeNode(1);
+
+            /* ------------------------------------ */
+            int targetSum = 22;
+            bool result = HasPathSum(root, targetSum);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ PathSum/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PathSum")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PathSum")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6800cb7f-1fef-4be5-9b8c-f87a0931ca6f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

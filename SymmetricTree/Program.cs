83ea5c3b33b783539d09f5e4e27e28600e5198f2
Minh Khoi Phan <pmka42@gmail.com>
2025-06-14 11:51:37 -0400

    Symmetric Tree (IsSameTree).

---
+++ SymmetricTree/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SymmetricTree
+{
+    internal class Program
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public static bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null)
+            {
+                return true;
+            }
+            if (p == null || q == null)
+            {
+                return false;
+            }
+            if (p.val != q.val)
+            {
+                return false;
+            }
+            return IsSameTree(p.left, q.right) && IsSameTree(p.right, q.left);
+        }
+
+        public static bool IsSymmetric(TreeNode root)
+        {
+            return IsSameTree(root.left, root.right);
+        }
+
+        static void Main(string[] args)
+        {
+            TreeNode root = new TreeNode(1, new TreeNode(2, new TreeNode(3), new TreeNode(4)), new TreeNode(2, new TreeNode(4), new TreeNode(3)));
+            bool result = IsSymmetric(root);
+            Console.WriteLine(result);
+        }
+    }
+}

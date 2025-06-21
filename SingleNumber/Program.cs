c2b57da2340f06415d15c002e8270705c5ea07b9
Minh Khoi Phan <pmka42@gmail.com>
2025-06-21 15:14:24 -0400

    Bitwise Operator XOR - Single Number.

---
+++ SingleNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SingleNumber
+{
+    internal class Program
+    {
+        private static int SingleNumber(int[] nums)
+        {
+            int result = 0;
+            foreach (int num in nums)
+            {
+                result ^= num;
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, 2, 3, 4, 3, 7, 7, };
+            Console.Write("XOR: " + SingleNumber(nums));
+        }
+    }
+}

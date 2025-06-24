12d82553bbccd878d9f46a273d0bb8fe17cf203e
Minh Khoi Phan <pmka42@gmail.com>
2025-06-24 17:28:55 -0400

    ConvertExcelColNumberToTitle.

---
+++ ExcelSheetColumnTitle/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelSheetColumnTitle
+{
+    internal class Program
+    {
+        private static string ConvertToTitle(int columnNumber)
+        {
+            const int BASE = 64;
+            StringBuilder title = new StringBuilder();
+            while (columnNumber > 0)
+            {                
+                int mod = columnNumber % 26;
+                if (mod == 0)
+                {
+                    columnNumber--;
+                    mod = 26;
+                }
+                char letter = (char)(mod + BASE);
+
+                title.Insert(0,letter);
+                columnNumber /= 26;
+            }
+            return title.ToString();
+        }
+        static void Main(string[] args)
+        {
+            string result = ConvertToTitle(28);
+            Console.WriteLine(result);
+        }
+    }
+}

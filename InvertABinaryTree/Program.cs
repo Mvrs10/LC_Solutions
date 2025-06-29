559bad5e4c5043399533fe6cf98ff71c6352507d
Minh Khoi Phan <pmka42@gmail.com>
2025-06-29 10:47:35 -0400

    Invert Binary Tree.

---
+++ InvertABinaryTree/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace InvertABinaryTree;
+using DataStructures;
+
+internal class Program
+{
+    private static void InvertNodes(TreeNode node)
+    {
+        if (node.left == null && node.right == null) return;
+        TreeNode temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+        InvertNodes(node.left);
+        InvertNodes(node.right);
+    }
+    private static TreeNode InvertTree(TreeNode root)
+    {
+        InvertNodes(root);
+        return root;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}

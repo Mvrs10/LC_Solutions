ff765892bba8d11b167bc63dba8a0864af718961
Minh Khoi Phan <pmka42@gmail.com>
2025-07-01 10:43:12 -0400

    Find The Original Typed String 1.

---
+++ FindTheOriginalTypedString1/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace FindTheOriginalTypedString1;
+
+internal class Program
+{
+    private static int PossibleStringCount(string word)
+    {
+        int count = 1;
+        for (int i = 0; i<word.Length -1; i++)
+        {
+            while (i < word.Length - 1 && word[i] == word[i + 1])
+            {
+                count++;
+                i++;
+            }
+        }
+        return count;
+    }
+
+    private static int PossibleStringCountFreqTable(string word)
+    {
+        int count = 1;
+        int[] freqTable = new int[26];
+        int BASE = 97;
+        int prev = word[0];
+        for (int i = 1; i < word.Length; i++)
+        {
+            if (prev == word[i])
+            {
+                freqTable[word[i] - BASE]++;
+            }
+            prev = word[i];
+        }
+        foreach (int i in freqTable)
+        {
+            if (i > 0)
+            {
+                count += i;
+            }
+        }
+        return count;
+    }
+    static void Main(string[] args)
+    {
+        string word1 = "ere", word2 = "abbcccc";        
+        int result = PossibleStringCount(word1);
+        Console.WriteLine(result);
+        result = PossibleStringCount(word2);
+        Console.WriteLine(result);
+        result = PossibleStringCountFreqTable(word1);
+        Console.WriteLine(result);
+        result = PossibleStringCountFreqTable(word2);
+        Console.WriteLine(result);
+    }
+}

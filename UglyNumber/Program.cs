53c19cdb2876f61d34e061851a0db1a3e596c827
Minh Khoi Phan <pmka42@gmail.com>
2025-07-03 16:05:38 -0400

    Ugly Number.

---
+++ UglyNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace UglyNumber;
+
+internal class Program
+{
+    private static bool IsUgly(int n)
+    {
+        if (n < 1) return false;
+        while (n > 1)
+        {
+            if (n % 2 == 0) n /= 2;
+            else if (n % 3 == 0) n /= 3;
+            else if (n % 5 == 0) n /= 5;
+            else return false;
+        }
+        return true;
+    }
+    static void Main(string[] args)
+    {
+        int a = 0;
+        int b = 6;
+        int c = 1;
+        int d = 14;
+        bool[] results = [IsUgly(a), IsUgly(b), IsUgly(c), IsUgly(d)];
+        foreach (bool r in results)
+        {
+            Console.WriteLine(r);
+        }
+    }
+}

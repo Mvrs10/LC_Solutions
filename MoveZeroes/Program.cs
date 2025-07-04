f359672ddeef08444ec90df2392548f6e57ee81b
Minh Khoi Phan <pmka42@gmail.com>
2025-07-04 11:47:24 -0400

    Move Zeroes.

---
+++ MoveZeroes/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace MoveZeroes;
+
+internal class Program
+{
+    private static void MoveZeroes(int[] nums)
+    {
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            if (nums[i] == 0)
+            {
+                int firstZeroIndex = i;
+                int checkpoint = firstZeroIndex;
+                while (i < nums.Length - 1 && firstZeroIndex == checkpoint)
+                {
+                    if (nums[++i] != 0)
+                    {
+                        nums[firstZeroIndex] = nums[i];
+                        nums[i] = 0;
+                        firstZeroIndex++;
+                    }
+                }
+                i = checkpoint;
+            }
+        }
+    }
+
+    private static void MoveZeroes_v2(int[] nums)
+    {
+        int zeroIndex = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != 0)
+            {
+                nums[zeroIndex++] = nums[i];
+            }
+        }
+        while (zeroIndex < nums.Length)
+        {
+            nums[zeroIndex++] = 0;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        int[] nums1 = [0, 1, 0, 3, 12];
+        MoveZeroes(nums1);
+        foreach (int i in nums1)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}

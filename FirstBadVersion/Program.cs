1f0f7d9aa761c6a371b482365f19facee9a1f2a6
Minh Khoi Phan <pmka42@gmail.com>
2025-07-04 10:32:04 -0400

    First Bad Version.

---
+++ FirstBadVersion/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace FirstBadVersion;
+
+internal class Program
+{
+    private static bool IsBadVersion(int n)
+    {
+        return n >= 4;
+    }
+
+    private static int FirstBadVersion(int n)
+    {
+        int first = 1;
+        int last = n;
+        while (first < last)
+        {
+            int mid = first + (last - first) / 2;
+            if (IsBadVersion(mid)) last = mid;
+            else first = mid + 1;
+        }
+        return first;
+    }
+    static void Main(string[] args)
+    {
+        int result = FirstBadVersion(9);
+        Console.WriteLine(result);
+    }
+}

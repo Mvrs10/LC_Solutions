edcd84d6b0c717238c3babf39657e319b6330b18
Minh Khoi Phan <pmka42@gmail.com>
2025-07-06 14:13:15 -0400

    Finding Pairs with a certain Sum.

---
+++ FindingPairsWithACertainSum/FindSumPairs.cs
@@ -0,0 +1,46 @@
+﻿namespace FindingPairsWithACertainSum;
+
+internal class FindSumPairs
+{
+    private readonly int[] _nums1;
+    private readonly int[] _nums2;
+    private readonly Dictionary<int, int> freqTable = new Dictionary<int,int>();
+
+    public FindSumPairs(int[] nums1, int[] nums2)
+    {
+        _nums1 = nums1;
+        Array.Sort(_nums1);
+        _nums2 = nums2;
+        foreach (int i in nums2)
+        {
+            if (!freqTable.ContainsKey(i)) freqTable[i] = 0;
+            freqTable[i]++;              
+        }
+    }
+
+    public void Add(int index, int val)
+    {
+        int v = _nums2[index];
+        _nums2[index] += val;
+        // Update freqTable
+        freqTable[v]--;
+        if (freqTable[v] == 0) freqTable.Remove(v);
+
+        v = _nums2[index];
+        if (!freqTable.ContainsKey(v)) freqTable[v] = 0;
+        freqTable[v]++;
+    }
+
+    public int Count(int tot)
+    {
+        int sumPairs = 0;
+        int index = 0;
+        while (index < _nums1.Length && _nums1[index] < tot)
+        {
+            int complement = tot - _nums1[index];
+            if(freqTable.ContainsKey(complement)) sumPairs += freqTable[complement];
+            index++;
+        }
+        return sumPairs;
+    }
+}
---
+++ FindingPairsWithACertainSum/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace FindingPairsWithACertainSum;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums1 = [1, 1, 2, 2, 2, 3];
+        int[] nums2 = [1, 4, 5, 2, 5, 4];
+        FindSumPairs obj = new FindSumPairs(nums1, nums2);
+        int r1 = obj.Count(7);
+        Console.WriteLine(r1);
+        obj.Add(3, 2);
+        r1 = obj.Count(8);
+        Console.WriteLine(r1);
+        r1 = obj.Count(4);
+        Console.WriteLine(r1);
+        obj.Add(0, 1);
+        obj.Add(1, 1);
+        r1 = obj.Count(11);
+        Console.WriteLine(r1);
+    }
+}

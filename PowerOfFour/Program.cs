f34d5c8dc3d245e796ce59b5afd1f82cb16467a8
Minh Khoi Phan <pmka42@gmail.com>
2025-07-07 15:30:26 -0400

    Power Of Four.

---
+++ PowerOfFour/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace PowerOfFour;
+
+internal class Program
+{
+    private static bool IsPowerOfFour(int n)
+    {
+        return n > 0 && (n & (n - 1)) == 0 && (n & 0x55555555) != 0;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine(IsPowerOfFour(16));
+        Console.WriteLine(IsPowerOfFour(1));
+        Console.WriteLine(IsPowerOfFour(132));
+    }
+}

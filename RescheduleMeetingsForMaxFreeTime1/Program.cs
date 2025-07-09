9ac07c4ef3cd4b8f8ee763f2d9d422ec147e3722
Minh Khoi Phan <pmka42@gmail.com>
2025-07-09 14:47:40 -0400

    Reschedule Meetings For Max Free Time 1.

---
+++ RescheduleMeetingsForMaxFreeTime1/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace RescheduleMeetingsForMaxFreeTime1;
+
+internal class Program
+{
+    private static int MaxFreeTime(int eventTime, int k, int[] startTime, int[] endTime)
+    {
+        int meetings = startTime.Length;
+        int[] gaps = new int[meetings + 1];
+
+        gaps[0] = startTime[0];
+        gaps[meetings] = eventTime - endTime[meetings - 1];
+
+        for (int i = 1; i < meetings; i++)
+        {
+            gaps[i] = startTime[i] - endTime[i - 1];
+        }
+
+        int freeTime = 0;
+        int windowSum = 0;
+        for (int i = 0; i < gaps.Length; i++) // Consecutive gaps
+        {
+            windowSum += gaps[i]; // Build the window
+            if (i >= k) freeTime = Math.Max(freeTime,windowSum); // Update max
+            if (i >= k + 1) windowSum -= gaps[i - k]; // Slide the window
+        }
+        return freeTime;
+    }
+    static void Main(string[] args)
+    {
+        int eventTime = 10;
+        int[] startTime = [0, 2, 9];
+        int[] endTime = [1, 4, 10];
+        int k = 1;
+
+        int result = MaxFreeTime(eventTime, k, startTime, endTime);
+        Console.WriteLine(result);
+    }
+}

5cf90429406a19f7a4995d920e5c7b230d2c6a1c
Minh Khoi Phan <pmka42@gmail.com>
2025-07-17 09:09:27 -0400

    Intersection Of Two Arrays 2.

---
+++ IntersectionOfTwoArrays2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace IntersectionOfTwoArrays2;
+
+internal class Program
+{
+    private static int[] Intersect(int[] nums1, int[] nums2)
+    {
+        Array.Sort(nums1);
+        Array.Sort(nums2);
+        List<int> result = new List<int>();
+        int n1 = 0, n2 = 0;
+        while (n1 < nums1.Length && n2 < nums2.Length)
+        {
+            if (nums1[n1] == nums2[n2])
+            {
+                result.Add(nums1[n1]);
+                n1++;
+                n2++;
+            }
+            else if (nums1[n1] < nums2[n2]) n1++;
+            else n2++;
+        }
+        return result.ToArray();
+    }
+    static void Main(string[] args)
+    {
+        int[] nums1 = [ 1, 2, 2, 1 ];
+        int[] nums2 = [ 2 ];
+        int[] result = Intersect(nums1, nums2);
+        foreach (int i in result)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}

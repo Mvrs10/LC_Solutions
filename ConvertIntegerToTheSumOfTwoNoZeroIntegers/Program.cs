1c63d64e2ce9555af66f2941ce962fb69cba8865
Minh Khoi Phan <pmka42@gmail.com>
2025-09-08 17:26:41 -0400

    Convert Integer to the Sum of Two No-Zero Integers.

---
+++ ConvertIntegerToTheSumOfTwoNoZeroIntegers/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ConvertIntegerToTheSumOfTwoNoZeroIntegers;
+
+internal class Program
+{
+    private static bool IsNoZeroInteger(int n)
+    {
+        while (n > 0)
+        {
+            if (n % 10 == 0)
+                return false;
+            n /= 10;
+        }
+        return true;
+    }
+    private static int[] GetNoZeroIntegers(int n)
+    {
+        int i = 1;
+        int j = n - i;
+        while (!IsNoZeroInteger(i) || !IsNoZeroInteger(j))
+        {
+            i++;
+            j--;
+        }
+
+        return [i, j];
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine(IsNoZeroInteger(590523));
+        int[] result = GetNoZeroIntegers(109);
+        foreach (int i in result)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}

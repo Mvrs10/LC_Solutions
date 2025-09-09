7a626046c12c6e7c5ff507da489ebb127e5c2f6d
Minh Khoi Phan <pmka42@gmail.com>
2025-09-09 12:18:05 -0400

    Largest 3 Same Digit Number in String.

---
+++ Largest3SameDigitNumberInString/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Largest3SameDigitNumberInString;
+
+internal class Program
+{
+    private static string LargestGoodInteger(string num)
+    {
+        List<string> GoodInts = new List<string> { "999", "888", "777", "666", "555", "444", "333", "222", "111", "000" };
+        foreach (string i in GoodInts)
+        {
+            if (num.Contains(i))
+                return i;
+        }
+        return "";
+    }
+    static void Main(string[] args)
+    {
+        string result = LargestGoodInteger("62227771398222");
+        Console.WriteLine(result);
+    }
+}

4d5cbd10219740c2108c23d13e2c959d11d8679f
Minh Khoi Phan <pmka42@gmail.com>
2025-09-11 10:46:03 -0400

    Sort Vowels In A String.

---
+++ SortVowelsInAString/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace SortVowelsInAString;
+
+internal class Program
+{
+    private static bool IsVowel(char c)
+    {
+        if (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' || c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+            return true;
+        return false;
+    }
+    private static string SortVowels(string s)
+    {
+        char[] vowels = new char[s.Length];
+        char[] t = new char[s.Length];
+        
+        for (int i = 0, j = 0; i < s.Length; i++)
+        {
+            if (IsVowel(s[i]))
+            {
+                vowels[j] = s[i];
+                j++;
+            }                
+            else
+                t[i] = s[i];
+        }
+
+        Array.Sort(vowels);
+
+        for (int i = t.Length - 1, j = vowels.Length - 1; i >= 0; i--)
+        {
+            if (t[i] == '\0')
+            {
+                t[i] = vowels[j];
+                j--;
+            }
+        }
+
+        return new string(t);
+    }
+    static void Main(string[] args)
+    {
+        string result = SortVowels("lEetcOde");
+        Console.WriteLine(result);
+    }
+}

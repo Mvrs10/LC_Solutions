fcd356db77e98a256e02770170c6ea450812c3a7
Minh Khoi Phan <pmka42@gmail.com>
2025-09-12 11:52:32 -0400

    Average Salary Excluding The Min And Max Salary.

---
+++ AverageSalaryExcludingTheMinAndMaxSalary/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace AverageSalaryExcludingTheMinAndMaxSalary;
+
+internal class Program
+{
+    private static double Average(int[] salary)
+    {
+        int min = salary[0];
+        int max = salary[0];
+        double sum = 0;
+        foreach (int s in salary)
+        {
+            if (s < min)
+                min = s;
+            if (s > max)
+                max = s;
+            sum += s;
+        }
+
+        return (sum - min - max) / (salary.Length - 2);
+    }
+
+    static void Main(string[] args)
+    {
+        double result = Average([4000, 3000, 1000, 2000]);
+        Console.WriteLine(result);
+    }
+}

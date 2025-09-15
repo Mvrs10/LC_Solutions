15956e74beff9f2547230024d5012aa631844533
Minh Khoi Phan <pmka42@gmail.com>
2025-09-15 11:16:53 -0400

    Maximum Number of Words Can Be Typed

---
+++ MaxiumumNumberOfWordsYouCanType/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace MaxiumumNumberOfWordsYouCanType;
+
+internal class Program
+{
+    private static int CanBeTypedWords(string text, string brokenLetters)
+    {
+        int count = 0;
+        string[] words = text.Split(' ');
+        char[] brokenChars = brokenLetters.ToCharArray();
+        foreach (string word in words)
+        {
+            foreach (char c in brokenChars)
+            {
+                if (word.Contains(c))
+                {
+                    count++;
+                    break;
+                }
+            }
+        }
+
+        return words.Length - count;
+    }
+    static void Main(string[] args)
+    {
+        int ans = CanBeTypedWords("leet code", "lt");
+        Console.WriteLine(ans);
+    }
+}

6bae63b9e6f070e11ee07c3cfa7592d25eac59fc
Minh Khoi Phan <pmka42@gmail.com>
2025-10-15 09:55:13 -0400

    Vowels Game In A String.

---
+++ VowelsGameInAString/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace VowelsGameInAString;
+
+internal class Program
+{
+    private static bool DoesAliceWin(string s)
+    {
+        bool IsVowel(char c)
+        {
+            if (c == 'a' ||  c == 'e' || c == 'i' || c == 'o' || c == 'u')
+            {
+                return true;
+            }
+            return false;
+        }
+
+        int count = 0;
+
+        foreach (char c in s)
+        {
+            if (IsVowel(c))
+                count++;
+        }
+
+        return count != 0;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}

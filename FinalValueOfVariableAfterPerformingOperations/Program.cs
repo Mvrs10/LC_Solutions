f598ee4c85f0241622231ff5262f45ef8f117766
Minh Khoi Phan <pmka42@gmail.com>
2025-10-23 12:04:16 -0400

    Final Value Of Variable After Performing Operations.

---
+++ FinalValueOfVariableAfterPerformingOperations/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace FinalValueOfVariableAfterPerformingOperations;
+
+internal class Program
+{
+    private static int FinalValueAfterOperations(string[] operations)
+    {
+        int x = 0;
+        foreach (string op in operations)
+        {
+            if (op.Contains('+'))
+            {
+                x++;
+            }
+            else
+            {
+                x--;
+            }
+        }
+        return x;
+    }
+    static void Main(string[] args)
+    {
+        string[] ops = ["--X", "X++", "X++"];
+        int result = FinalValueAfterOperations(ops);
+        Console.WriteLine(result);
+    }
+}

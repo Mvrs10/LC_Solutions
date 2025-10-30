57ca04516afdd57824e0a2b2ecdc5cb6ceaa96cf
Minh Khoi Phan <pmka42@gmail.com>
2025-10-30 13:24:58 -0400

    Minimum Number Of Increments On Subarray ToF  rm A Target Array.

---
+++ MinimumNumberOfIncrementsOnSubarrayToFormATargetArray/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace MinimumNumberOfIncrementsOnSubarrayToFormATargetArray;
+
+internal class Program
+{
+    private static int MinimumOperations(int[] target)
+    {
+        int ops = target[0];
+
+        for (int i = 1; i < target.Length; i++)
+        {
+            if (target[i] > target[i - 1])
+            {
+                ops += target[i] - target[i - 1];
+            }
+        }
+
+        return ops;
+    }
+    static void Main(string[] args)
+    {
+        int[] target = [1, 2, 3, 2, 1];
+        int result = MinimumOperations(target);
+        Console.WriteLine(result);
+    }
+}

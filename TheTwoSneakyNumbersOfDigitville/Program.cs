eb83cfabf7ce251a29a487a6995df6e31c93c12b
Minh Khoi Phan <pmka42@gmail.com>
2025-10-31 17:50:43 -0400

    The Two Sneaky Numbers Of Digitville.

---
+++ TheTwoSneakyNumbersOfDigitville/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace TheTwoSneakyNumbersOfDigitville;
+
+internal class Program
+{
+    private static int[] GetSneakyNumber(int[] nums)
+    {
+        int[] result = new int[2];
+        int[] count = new int[100];
+        int index = 0;
+
+        foreach(int num in nums)
+        {
+            count[num]++;
+
+            if (count[num] == 2)
+            {
+                result[index] = num;
+                index++;
+            }
+            if (index == 2)
+                break;
+        }
+
+        return result;
+    }
+    static void Main(string[] args)
+    {
+        int[] nums = [1, 0, 1, 0];
+        int[] result = GetSneakyNumber(nums);
+        foreach (int n in result)
+        {
+            Console.WriteLine(n);
+        }
+    }
+}

15532f9d9d6ef10a74529d1fb88b1b191a46ac96
Minh Khoi Phan <pmka42@gmail.com>
2025-11-09 15:02:06 -0500

    Minimum One Bit Operations To Make Integers Zero.

---
+++ MinimumOneBitOperationsToMakeIntegersZero/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace MinimumOneBitOperationsToMakeIntegersZero;
+
+internal class Program
+{
+    private static int minimumOneBitOperations(int n)
+    {
+        int result = 0;
+        while (n > 0)
+        {
+            result ^= n;
+            n >>= 1;
+        }
+        return result;
+    }
+    static void Main(string[] args)
+    {
+        int result = minimumOneBitOperations(15);
+        Console.WriteLine(result);
+    }
+}

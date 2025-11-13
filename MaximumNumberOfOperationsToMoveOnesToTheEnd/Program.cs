984a7d26fff143c0c49d6346d30e9f08fcfd945c
Minh Khoi Phan <pmka42@gmail.com>
2025-11-13 13:26:32 -0500

    Maximum Number of Operations To Move Ones to the End.

---
+++ MaximumNumberOfOperationsToMoveOnesToTheEnd/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace MaximumNumberOfOperationsToMoveOnesToTheEnd;
+
+internal class Program
+{
+    private static int MaxOperations(string s)
+    {
+        int ops = 0, ones = 0;
+
+        for(int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == '0')
+            {
+                while (i < s.Length && s[i] == '0')
+                    i++;
+                ops += ones;
+            }
+            ones++;
+        }
+
+        return ops;
+    }
+    static void Main(string[] args)
+    {
+        string s1 = "1001101";
+        string s2 = "00111";
+        string s3 = "0011010101100";
+        string[] s = { s1, s2, s3 };
+
+        foreach (string ss in s)
+        {
+            Console.WriteLine(MaxOperations(ss));
+        }
+    }
+}

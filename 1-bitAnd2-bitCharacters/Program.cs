ef3b115faea62d4993db358de41657427fbe2e17
Minh Khoi Phan <pmka42@gmail.com>
2025-11-18 00:01:56 -0500

    1-bit and 2-bit Characters.

---
+++ 1-bitAnd2-bitCharacters/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _1_bitAnd2_bitCharacters;
+
+internal class Program
+{
+    private static bool IsOneBitCharacter(int[] bits)
+    {
+        bool isOneBit = true;
+        int i = 0;
+        while (i < bits.Length)
+        {
+            if (bits[i] == 0)
+            {
+                isOneBit = true;
+                i++;
+            }
+            else
+            {
+                isOneBit = false;
+                i += 2;
+            }
+        }
+
+        return isOneBit;
+    }
+    static void Main(string[] args)
+    {
+        int[] bits = [1,1,1,0];
+        bool result = IsOneBitCharacter(bits);
+        Console.WriteLine(result);
+    }
+}

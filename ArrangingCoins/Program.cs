c38767c99a8b7c7c5177624f6578220e87fe2e35
Minh Khoi Phan <pmka42@gmail.com>
2025-11-22 13:21:08 -0500

    Arranging Coins.

---
+++ ArrangingCoins/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ArrangingCoins;
+
+internal class Program
+{
+    private static int ArrangeCoins(int n)
+    {
+        int row = 0;
+        int i = 1;
+
+        while (n > 0)
+        {
+            n -= i;
+            i++;
+            row++;
+        }
+
+        return (n == 0) ? row : row - 1;
+    }
+
+    private static int ArrangeCoins_v2(int n)
+    {
+        return (int)(-1 + Math.Sqrt(1 + 8.0*n)) / 2;
+    }
+
+    static void Main(string[] args)
+    {
+        int result = ArrangeCoins(6);
+        Console.WriteLine(result);
+        result = ArrangeCoins(5);
+        Console.WriteLine(result);
+        result = ArrangeCoins_v2(6);
+        Console.WriteLine(result);
+        result = ArrangeCoins_v2(5);
+        Console.WriteLine(result);
+    }
+}

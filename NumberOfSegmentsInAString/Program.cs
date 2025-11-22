041fc9ae0c8c8a0c783dd3d3bf1ca67fd6920478
Minh Khoi Phan <pmka42@gmail.com>
2025-11-22 12:36:31 -0500

    Number Of Segments In A String.

---
+++ NumberOfSegmentsInAString/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace NumberOfSegmentsInAString;
+
+internal class Program
+{
+    private static int CountSegments(string s)
+    {
+        int count = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == ' ')
+                continue;
+            while (i < s.Length && s[i] != ' ')
+            {
+                i++;
+            }
+            count++;
+        }
+
+        return count;
+    }
+    static void Main(string[] args)
+    {
+        string s = "Hello, my name is John";
+        int result = CountSegments(s);
+        Console.WriteLine(result);
+        s = " aa  ,bbbb     k    ";
+        result = CountSegments(s);
+        Console.WriteLine(result);
+    }
+}

7fbe5e4c90e7fc71e422f793238d11912d6685fd
Minh Khoi Phan <pmka42@gmail.com>
2025-11-21 22:50:08 -0500

    Find Minimum Operations To Make All Elements Divisible By Three.

---
+++ FindMinimumOperationsToMakeAllElementsDivisibleByThree/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace FindMinimumOperationsToMakeAllElementsDivisibleByThree;
+
+internal class Program
+{
+    private static int MinimumOperations(int[] nums)
+    {
+        int count = 0;
+
+        foreach (int n in nums)
+        {
+            if (n % 3 != 0)
+                count++;
+        }
+
+        return count;
+    }
+    static void Main(string[] args)
+    {
+        int[] nums = [1, 2, 3, 4];
+        int result = MinimumOperations(nums);
+        Console.WriteLine(result);
+    }
+}

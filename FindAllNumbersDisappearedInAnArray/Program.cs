21180d290623427fd666b7f63e98e8d586b8f6eb
Minh Khoi Phan <pmka42@gmail.com>
2025-11-23 11:57:24 -0500

    Find All Numbers Disappeared In An Array.

---
+++ FindAllNumbersDisappearedInAnArray/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace FindAllNumbersDisappearedInAnArray;
+
+internal class Program
+{
+    private static IList<int> FindDisappearedNumbers(int[] nums)
+    {
+        IList<int> missingNumbers = new List<int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int j = nums[i];
+            while (nums[i] != i + 1 && nums[j - 1] != j && nums[i] != nums[j - 1])
+            {
+                int temp = nums[j - 1];
+                nums[j - 1] = j;
+                nums[i] = temp;
+                j = nums[i];
+            }
+        }
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != i + 1)
+                missingNumbers.Add(i + 1);
+        }
+
+        return missingNumbers;
+    }
+    static void Main(string[] args)
+    {
+        int[] nums = [4, 3, 2, 7, 8, 2, 3, 1];
+        IList<int> result = FindDisappearedNumbers(nums);
+        foreach(int i in result)
+        {
+            Console.Write($"{i} ");
+        }
+    }
+}

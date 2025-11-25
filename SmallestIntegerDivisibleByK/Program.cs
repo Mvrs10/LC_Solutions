5b45a811a4b4ae1b603e8c44c44cd0b76546af94
Minh Khoi Phan <pmka42@gmail.com>
2025-11-25 16:56:26 -0500

    Smallest Integer Divisible By K.

---
+++ SmallestIntegerDivisibleByK/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace SmallestIntegerDivisibleByK;
+
+internal class Program
+{
+    private static int SmallestRepunitDivByK(int k)
+    {
+        if (k % 2 == 0 || k % 5 == 0)
+            return -1;
+
+        int remainder = 0;
+        for (int i = 1; i <= k; i++)
+        {
+            remainder = (remainder * 10 + 1) % k;
+            if (remainder == 0)
+                return i;
+        }
+        return -1;
+    }
+
+    static void Main(string[] args)
+    {
+        int K = 3;
+        int result = SmallestRepunitDivByK(K);
+        Console.WriteLine(result);
+    }
+}

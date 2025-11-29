21aeaee5534bf8de4b52a511357420a24f8ae488
Minh Khoi Phan <pmka42@gmail.com>
2025-11-29 18:34:26 -0500

    Longest Substring Without Repeating Characters.

---
+++ LongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace LongestSubstringWithoutRepeatingCharacters;
+
+internal class Program
+{
+    private static int LengthOfLongestSubstring(string s)
+    {
+        int n = s.Length;
+        int l = 0;
+        HashSet<char> seenChars = new HashSet<char>();
+
+        for (int i = 0, j = 0; j < n; i++, j++)
+        {
+            while (j < n && !seenChars.Contains(s[j]))
+            {
+                seenChars.Add(s[j++]);
+                l = Math.Max(l, j - i);
+            }
+
+            while (j < n && s[i] != s[j])
+            {
+                seenChars.Remove(s[i]);
+                i++;
+            }
+        }
+
+        return l;
+    }
+
+    static void Main(string[] args)
+    {
+        string[] testCases = ["tmmzuxt", "abcabcbb", "bbb", "pwwekw", "abcdaefghijklmn","abbbbbb"];
+
+        foreach (string s in testCases)
+        {
+            Console.WriteLine(LengthOfLongestSubstring(s));
+        }
+    }
+}

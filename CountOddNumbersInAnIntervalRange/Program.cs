d2701b8ddbb18125fb02ba2c5c1657d53bdd0c07
Minh Khoi Phan <pmka42@gmail.com>
2025-12-08 11:40:23 -0500

    Count Odd Numbers in an Interval Range.

---
+++ CountOddNumbersInAnIntervalRange/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace CountOddNumbersInAnIntervalRange;
+
+internal class Program
+{
+    private static int CountOdds(int low, int high)
+    {
+        int oddCount = 0;
+        if ((low % 2) != 0)
+        {
+            oddCount++;
+            low++;
+        }
+        if ((high % 2) != 0)
+        {
+            oddCount++;
+            high--;
+        }
+
+        oddCount += ((high - low) / 2);
+        return oddCount;
+    }
+    static void Main(string[] args)
+    {
+        int[][] testCases = [[3, 7], [8, 10], [1, 2], [9, 9], [10, 10]];
+        foreach (int[] test in testCases)
+        {
+            int result = CountOdds(test[0], test[1]);
+            Console.WriteLine(result);
+        }
+    }
+}

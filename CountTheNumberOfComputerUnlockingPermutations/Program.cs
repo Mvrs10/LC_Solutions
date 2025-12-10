db782060342b04b00560d63f6f93944e3079ba4c
Minh Khoi Phan <pmka42@gmail.com>
2025-12-10 08:59:55 -0500

    Count The Number of Computer Unlocking Permutations.

---
+++ CountTheNumberOfComputerUnlockingPermutations/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CountTheNumberOfComputerUnlockingPermutations;
+
+internal class Program
+{
+    private static int CountPermutations(int[] complexity)
+    {
+        long permutation = 1;
+
+        for (int i = 1; i < complexity.Length; i++)
+        {
+            if (complexity[i] <= complexity[0])
+                return 0;
+
+            permutation = (permutation * i) % 1_000_000_007;
+        }
+
+        return (int)permutation;
+    }
+
+    static void Main(string[] args)
+    {
+        int[] complexity = [4, 6, 6, 6, 6, 6, 6, 6, 6, 6];
+        int result = CountPermutations(complexity);
+        Console.WriteLine(result);
+    }
+}

0fd32acd33672210376411155a46d669de8c63e2
Minh Khoi Phan <pmka42@gmail.com>
2026-01-02 12:05:08 -0500

    N Repeated Element Size Array.

---
+++ NRepeatedElementInSize2NArray/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace NRepeatedElementInSize2NArray;
+
+internal class Program
+{
+    private static int RepeatedNTimes(int[] nums)
+    {
+        Dictionary<int,int> track = new Dictionary<int,int>();
+
+        foreach (int n in nums)
+        {
+            if (!track.ContainsKey(n))
+            {
+                track[n] = 0;
+            }
+            track[n]++;
+            if (track[n] == nums.Length / 2)
+                return n;
+        }
+
+        return -1;
+    }
+
+    static void Main(string[] args)
+    {
+        int[] nums = [5, 1, 5, 2, 5, 3, 5, 4];
+        int result = RepeatedNTimes(nums);
+        Console.WriteLine(result);
+    }
+}

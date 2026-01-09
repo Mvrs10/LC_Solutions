736649a0bccc7ec58d9f5ddbb9f41ba66304ecc4
Minh Khoi Phan <pmka42@gmail.com>
2026-01-09 03:58:21 -0500

    Smallest Subtree With All The Deepest Nodes.

---
+++ SmallestSubtreeWithAllTheDeepestNodes/Program.cs
@@ -0,0 +1,38 @@
+﻿using DataStructures;
+namespace SmallestSubtreeWithAllTheDeepestNodes;
+
+internal class Program
+{
+    private static TreeNode SubtreeWithAllDeepest(TreeNode root)
+    {
+        int maxDepth = GetMaxDepth(root);
+        return GetSmallestNode(root, 1, maxDepth);
+    }
+
+    private static int GetMaxDepth(TreeNode node)
+    {
+        if (node == null) return 0;
+
+        return 1 + Math.Max(GetMaxDepth(node.left), GetMaxDepth(node.right));
+    }
+
+    private static TreeNode GetSmallestNode(TreeNode node, int depth, int maxDepth)
+    {
+        if (node == null) return null;
+
+        if (depth == maxDepth) return node;
+
+        TreeNode left = GetSmallestNode(node.left, depth + 1, maxDepth);
+        TreeNode right = GetSmallestNode(node.right, depth + 1, maxDepth);
+
+        if (left != null && right != null) return node;
+        return left ?? right;
+    }
+
+    static void Main(string[] args)
+    {
+        TreeNode root = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+        TreeNode result = SubtreeWithAllDeepest(root);
+        Console.WriteLine(result.val);
+    }
+}

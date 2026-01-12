5b4451921d5d38a67c888099493b1cc83e421e1d
Minh Khoi Phan <pmka42@gmail.com>
2026-01-12 02:59:56 -0500

    Minimum Time Visiting All Points.

---
+++ MinimumTimeVisitingAllPoints/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace MinimumTimeVisitingAllPoints;
+
+internal class Program
+{
+    private static int MinTimeToVisitAllPoints(int[][] points)
+    {
+        int time = 0;
+        for (int i = 1; i < points.Length; i++)
+        {
+            int x = Math.Abs(points[i][0] - points[i - 1][0]);
+            int y = Math.Abs(points[i][1] - points[i - 1][1]);
+
+            time += (x < y) ? y : x;
+        }
+
+        return time;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}

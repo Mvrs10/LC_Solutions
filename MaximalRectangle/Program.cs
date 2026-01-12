fc609488245ff138eb5e8073d1409309fd31b8e4
Minh Khoi Phan <pmka42@gmail.com>
2026-01-12 00:18:53 -0500

    Maximal Rectangle.

---
+++ MaximalRectangle/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace MaximalRectangle;
+
+internal class Program
+{
+    private static int MaximalRectangle(char[][] matrix)
+    {
+        if (matrix == null || matrix.Length == 0 || matrix[0].Length == 0)
+            return 0;
+
+        int rows = matrix.Length;
+        int cols = matrix[0].Length;
+        int[] heights = new int[cols];
+        int maxArea = 0;
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                if (matrix[i][j] == '1')
+                {
+                    heights[j] += 1;
+                }
+                else
+                {
+                    heights[j] = 0;
+                }
+            }
+
+            maxArea = Math.Max(maxArea, LargestRectangleArea(heights));
+        }
+
+        return maxArea;
+    }
+    private static int LargestRectangleArea(int[] heights)
+    {
+        Stack<int> stack = new Stack<int>();
+        int maxArea = 0;
+        int n = heights.Length;
+
+        for (int i = 0; i <= n; i++)
+        {
+            int currentHeight = (i == n) ? 0 : heights[i];
+            while (stack.Count > 0 && currentHeight < heights[stack.Peek()])
+            {
+                int height = heights[stack.Pop()];
+                int width = (stack.Count == 0) ? i : i - stack.Peek() - 1;
+                maxArea = Math.Max(maxArea, height * width);
+            }
+            stack.Push(i);
+        }
+
+        return maxArea;
+    }
+    static void Main(string[] args)
+    {
+        char[][] matrix = [['1', '0', '1', '0', '0'], ['1', '0', '1', '1', '1'], ['1', '1', '1', '1', '1'], ['1', '0', '0', '1', '0']];
+        int result = MaximalRectangle(matrix);
+        Console.WriteLine(result);
+    }
+}

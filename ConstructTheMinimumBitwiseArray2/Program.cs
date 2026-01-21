e95a96f726faa3e47053bac2bbebbb09ab481d52
Minh Khoi Phan <pmka42@gmail.com>
2026-01-21 12:56:14 -0500

    Construct the Minimum Bitwise Array 2.

---
+++ ConstructTheMinimumBitwiseArray2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConstructTheMinimumBitwiseArray2;
+
+internal class Program
+{
+    private static int[] MinBitwiseArray(IList<int> nums)
+    {
+        for (int i = 0; i < nums.Count; i++)
+        {
+            int x = -1;
+            int d = 1;
+            while ((nums[i] & d) != 0)
+            {
+                x = nums[i] - d;
+                d <<= 1;
+            }
+            nums[i] = x;
+        }
+
+        return nums.ToArray();
+    }
+    static void Main(string[] args)
+    {
+        IList<int> nums = [2, 3, 5, 7];
+        int[] result = MinBitwiseArray(nums);
+        foreach (int n in result)
+        {
+            Console.Write(n + " ");
+        }
+    }
+}

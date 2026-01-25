e75bbc89e03a960308b59150e993f59a52b11bae
Minh Khoi Phan <pmka42@gmail.com>
2026-01-25 15:46:37 -0500

    Minimum Difference Between Highest and Lowest of K Scores.

---
+++ MinimumDifferenceBetweenHighestandLowestofKScores/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace MinimumDifferenceBetweenHighestandLowestofKScores;
+
+internal class Program
+{
+    private static int MinimumDifferences(int[] nums, int k)
+    {
+        if (k == 1) return 0;
+
+        Array.Sort(nums);
+        int diff = 100000;
+        for(int i = 0; i + k - 1 < nums.Length; i++)
+        {
+            diff = Math.Min(diff, nums[i + k - 1] - nums[i]);
+        }
+
+        return diff;
+    }
+    static void Main(string[] args)
+    {
+        int[] nums = [9, 4, 7, 1];
+        int result = MinimumDifferences(nums, 2);
+        Console.WriteLine(result);
+    }
+}

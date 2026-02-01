bf18f27cd2ed6fe755b827e181cd419ba1def959
Minh Khoi Phan <pmka42@gmail.com>
2026-02-01 01:13:31 -0500

    Divide An Array Into Subarrays With Minimum Cost 1.

---
+++ DivideAnArrayIntoSubarraysWithMinimumCost1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace DivideAnArrayIntoSubarraysWithMinimumCost1;
+
+internal class Program
+{
+    private static int MinimumCost(int[] nums)
+    {
+        int n = nums[0];
+        nums[0] = 50;
+
+        Array.Sort(nums);
+
+        return n + nums[0] + nums[1];
+    }
+
+    static void Main(string[] args)
+    {
+        int[] nums = [10, 3, 1, 1];
+        int result = MinimumCost(nums);
+        Console.WriteLine(result);
+    }
+}

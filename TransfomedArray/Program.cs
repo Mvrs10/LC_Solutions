978c7b70c12cd4eca78c3b926e09e05337c211ef
Minh Khoi Phan <pmka42@gmail.com>
2026-02-04 22:59:04 -0500

    Transformed Array.

---
+++ TransfomedArray/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace TransfomedArray;
+
+internal class Program
+{
+    private static int[] ConstructTransformedArray(int[] nums)
+    {
+        int n = nums.Length;
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            int count = nums[i] < 0 ? -nums[i] : nums[i];
+            int j = i;
+            while (count > 0)
+            {
+                if (nums[i] < 0)
+                    j--;
+                else
+                    j++;
+
+                if (j == n)
+                    j = 0;
+                if (j == -1)
+                    j = n - 1;
+
+                count--;
+            }
+            result[i] = nums[j];
+        }
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        int[] nums = [3, -2, 1, 1];
+        int[] result = ConstructTransformedArray(nums);
+        foreach (int i in result)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}

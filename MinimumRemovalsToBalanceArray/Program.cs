974589364c312debb4c8ef95236ea2335b123bd9
Minh Khoi Phan <pmka42@gmail.com>
2026-02-06 11:07:07 -0500

    Minimum Removals To Balance Array.

---
+++ MinimumRemovalsToBalanceArray/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace MinimumRemovalsToBalanceArray;
+
+internal class Program
+{
+    private static int MinRemoval(int[] nums, int k)
+    {
+        int min = nums.Length;
+        Array.Sort(nums);
+
+        for (int i = 0, j = 0; j < nums.Length; j++)
+        {
+            while (i < j && (long)nums[i] * k < nums[j])
+            {
+                i++;
+            }
+
+            min = Math.Min(min, nums.Length - (j - i + 1));
+        }
+
+        return min;
+    }
+
+    static void Main(string[] args)
+    {
+        int k = 3;
+        int[] nums = [1, 6, 2, 9];
+
+        int result = MinRemoval(nums, k);
+        Console.WriteLine(result);
+    }
+}

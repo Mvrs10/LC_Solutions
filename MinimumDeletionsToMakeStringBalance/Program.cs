12da61f1a4a57531387a099a83fd025a486f7123
Minh Khoi Phan <pmka42@gmail.com>
2026-02-07 11:18:31 -0500

    Minimum Deletions to Make String Balanced.

---
+++ MinimumDeletionsToMakeStringBalance/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace MinimumDeletionsToMakeStringBalance;
+
+internal class Program
+{
+    private static int MinimumDeletions(string s)
+    {
+        int aCount = 0, bCount = 0;
+
+        foreach (char c in s)
+        {
+            if (c == 'a')
+                aCount++;
+        }
+
+        int i = 0;
+        int min = aCount;
+        while (i < s.Length)
+        {
+            if (s[i] == 'a')
+                aCount--;
+            else
+                bCount++;
+            min = Math.Min(min, aCount + bCount);
+            i++;
+        }
+
+        return min;
+    }
+    static void Main(string[] args)
+    {
+        string s = "bbbbbbbaab";
+        int result = MinimumDeletions(s);
+        Console.WriteLine(result);
+    }
+}

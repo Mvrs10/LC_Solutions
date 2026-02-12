18c16c9e5d072337fbba4eb85af730d520aa6c20
Minh Khoi Phan <pmka42@gmail.com>
2026-02-12 18:50:44 -0500

    Longest Balanced Substring 1.

---
+++ LongestBalancedSubstring1/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace LongestBalancedSubstring1;
+
+internal class Program
+{
+    private static int LongestBalanced(string s)
+    {
+        int max = 0;        
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            Dictionary<char, int> map = new Dictionary<char, int>();
+            for (int j = i; j < s.Length; j++)
+            {
+                if (!map.ContainsKey(s[j]))
+                    map[s[j]] = 0;
+                map[s[j]]++;
+
+                int count = map.Values.First();
+                bool isBalanced = true;
+                foreach (KeyValuePair<char, int> kvp in map)
+                {
+                    if (kvp.Value != count)
+                    {
+                        isBalanced = false;
+                    }
+                }
+                if (isBalanced)
+                    max = Math.Max(max, j - i + 1);
+            }
+        }
+
+        return max;
+    }
+    static void Main(string[] args)
+    {
+        string[] tests = ["a", "abbac", "zzabccy", "aba", "aaaaaa"];
+        foreach (string t in tests)
+        {
+            int result = LongestBalanced(t);
+            Console.WriteLine(result);
+        }
+    }
+}

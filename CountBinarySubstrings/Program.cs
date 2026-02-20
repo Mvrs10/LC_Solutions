d994d6b62617a1a7434a3b0d4ed590e67cf93241
Minh Khoi Phan <pmka42@gmail.com>
2026-02-19 23:16:17 -0500

    Count Binary Substrings.

---
+++ CountBinarySubstrings/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace CountBinarySubstrings;
+
+internal class Program
+{
+    private static int CountBinarySubstrings(string s)
+    {
+        int result = 0;
+        int prev = 0;
+        int current = 1;
+
+        for (int i = 1; i < s.Length; i++)
+        {
+            if (s[i] == s[i - 1])
+            {
+                current++;
+            }
+            else
+            {
+                result += Math.Min(prev, current);
+                prev = current;
+                current = 1;
+            }
+        }
+
+        result += Math.Min(prev, current);
+
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        string s = "00110011";
+        int result = CountBinarySubstrings(s);
+        Console.WriteLine(result);
+    }
+}

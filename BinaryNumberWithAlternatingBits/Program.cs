9565082a3dd6b8152c4ee6d0391842bc11317514
Minh Khoi Phan <pmka42@gmail.com>
2026-02-20 00:10:24 -0500

    Binary Number With Alternating Bits.

---
+++ BinaryNumberWithAlternatingBits/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace BinaryNumberWithAlternatingBits;
+
+internal class Program
+{
+    private static bool hasAlternatingBits(int n)
+    {
+        int lsb = n & 1;
+        n >>= 1;
+        
+        while (n > 0)
+        {
+            if ((n & 1) == lsb) return false;
+            lsb = n & 1;
+            n >>= 1;
+        }
+        return true;
+    }
+
+    static void Main(string[] args)
+    {
+        int n = 5;
+        bool result = hasAlternatingBits(n);
+        Console.WriteLine(result);
+    }
+}

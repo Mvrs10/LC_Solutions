19caa5dd2d6550b7fe075e313d3b5379c4c60f59
Minh Khoi Phan <pmka42@gmail.com>
2026-02-24 23:14:03 -0500

    Check If a String Contains All Binary Codes of Size K.

---
+++ CheckIfAStringContainsAllBinaryCodesOfSizeK/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace CheckIfAStringContainsAllBinaryCodesOfSizeK;
+
+internal class Program
+{
+    private static bool HasAllCodes(string s, int k)
+    {
+        HashSet<string> binaryCodes = new HashSet<string>();
+        int all = 1 << k;
+        for (int i = 0; i + k <= s.Length; i++)
+        {
+            string binaryCode = s.Substring(i, k);
+            binaryCodes.Add(binaryCode);
+            if (binaryCodes.Count == all) return true;
+        }
+
+        return false;
+    }
+    static void Main(string[] args)
+    {
+        string s = "00110110";
+        int k = 2;
+        bool result = HasAllCodes(s, k);
+        Console.Write(result);
+    }
+}

74e9d541b6a693367918facf1b14fb7a3e7c0861
Minh Khoi Phan <pmka42@gmail.com>
2026-03-02 16:30:01 -0500

    Partitioning Into Minimum Number of Deci-Binary Numbers.

---
+++ PartitioningIntoMinimumNumberOfDeciBinaryNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace PartitioningIntoMinimumNumberOfDeciBinaryNumbers;
+
+internal class Program
+{
+    private static int MinPartitions(string n)
+    {
+        int max = 1;
+
+        foreach(char c in n)
+        {
+            max = Math.Max(max, c - '0');
+        }
+
+        return max;
+    }
+    static void Main(string[] args)
+    {
+        string n = " 128021";
+        int result = MinPartitions(n);
+        Console.WriteLine(result);
+    }
+}

dcb4970d4f86c499f87ccb21998bf7a872b895a9
Minh Khoi Phan <pmka42@gmail.com>
2026-03-05 18:39:36 -0500

    Minimum Changes to Make Alternating Binary String.

---
+++ MinimumChangesToMakeAlternatingBinaryString/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace MinimumChangesToMakeAlternatingBinaryString;
+
+internal class Program
+{
+    private static int MinOperations(string s)
+    {
+        int oneZero = 0;
+        int zeroOne = 0;
+
+        char current = '0';
+        foreach (char c in s)
+        {
+            if (c == current)
+            { 
+                oneZero++;
+            }
+            current = (current == '1') ? '0' : '1';
+        }
+
+        current = '1';
+        foreach (char c in s)
+        {
+            if (c == current)
+            {
+                zeroOne++;
+            }
+            current = (current == '1') ? '0' : '1';
+        }
+
+        return Math.Min(oneZero, zeroOne);
+    }
+
+    static void Main(string[] args)
+    {
+        string s = "100001";
+        int result = MinOperations(s);
+        Console.WriteLine(result);
+    }
+}

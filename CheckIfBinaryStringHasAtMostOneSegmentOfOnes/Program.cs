6ad57c7fa48b64b4425684cc790a7b5da12bb205
Minh Khoi Phan <pmka42@gmail.com>
2026-03-06 00:45:22 -0500

    Check If Binray String Has At Most One Segment of Ones.

---
+++ CheckIfBinaryStringHasAtMostOneSegmentOfOnes/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace CheckIfBinaryStringHasAtMostOneSegmentOfOnes;
+
+internal class Program
+{
+    private static bool CheckOnesSegment(string s)
+    {
+        int n = s.Length;
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == '0')
+            {
+                while (i < n)
+                {
+                    if (s[i] == '1') return false;
+                    i++;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    static void Main(string[] args)
+    {
+        string s = "1100001000";
+        bool result = CheckOnesSegment(s);
+        Console.WriteLine(result);
+    }
+}

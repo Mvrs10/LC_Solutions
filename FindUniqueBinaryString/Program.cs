221ff8031d0c98dd1ca91be794ba84d6324708e7
Minh Khoi Phan <pmka42@gmail.com>
2026-03-08 22:08:53 -0400

    Find Unique Binary String.

---
+++ FindUniqueBinaryString/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace FindUniqueBinaryString;
+
+internal class Program
+{
+    private static string FindDifferentBinaryString(string[] nums)
+    {
+        int n = nums.Length;
+        char[] unique = new char[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            unique[i] = nums[i][i] == '0' ? '1' : '0';
+        }
+
+        return new string(unique);
+    }
+
+    static void Main(string[] args)
+    {
+        string[] nums = ["01", "10"];
+        string result = FindDifferentBinaryString(nums);
+        Console.WriteLine(result);
+    }
+}

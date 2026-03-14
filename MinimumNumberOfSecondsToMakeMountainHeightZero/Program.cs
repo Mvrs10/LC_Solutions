1b8f65f1681935c75b08a6311daaa9018db370e2
Minh Khoi Phan <pmka42@gmail.com>
2026-03-13 23:11:17 -0400

    Minimum Number Of Seconds To Make Mountain Height Zero.

---
+++ MinimumNumberOfSecondsToMakeMountainHeightZero/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace MinimumNumberOfSecondsToMakeMountainHeightZero;
+
+internal class Program
+{
+    private static long MinNumberOfSeconds(int mountainHeight, int[] workerTimes)
+    {
+        long left = 1;
+        long right = (long)workerTimes.Min() * mountainHeight * (mountainHeight + 1) / 2;
+
+        while (left < right)
+        {
+            long mid = (left + right) / 2;
+
+            if (CanFinish(mid, mountainHeight, workerTimes))
+            {
+                right = mid;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    private static bool CanFinish(long time, int mountainHeight, int[] workerTimes)
+    {
+        long removed = 0;
+
+        foreach (int t in workerTimes)
+        {
+            long x = (long)((Math.Sqrt(1 + 8.0 * time / t) - 1) / 2);
+            removed += x;
+
+            if (removed >= mountainHeight) return true;
+        }
+
+        return false;
+    }
+
+    static void Main(string[] args)
+    {
+        int mountainHeight = 4;
+        int[] workerTimes = [2, 1, 1];
+
+        long result = MinNumberOfSeconds(mountainHeight, workerTimes);
+
+        Console.WriteLine(result);
+    }
+}

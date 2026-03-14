4fb46cc4945e08c6833d3680c27f2347d786f60a
Minh Khoi Phan <pmka42@gmail.com>
2026-03-14 18:32:52 -0400

    The K_th Lexicographical String Of All Happy Strings Of Length N.

---
+++ TheK-thLexicographicalStringOfAllHappyStringsOfLengthN/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace TheK_thLexicographicalStringOfAllHappyStringsOfLengthN;
+
+internal class Program
+{
+    private static List<string> HappyStrings = new List<string>();
+    private static char[] HappyCharacters = ['a', 'b', 'c'];
+
+    private static string GetHappyString(int n, int k)
+    {
+        string s = "";
+        BuildHappyString(s, n);
+        if (HappyStrings.Count < k) return "";
+        return HappyStrings[k - 1];
+    }
+
+    private static void BuildHappyString(string s, int n)
+    {
+        if (s.Length == n)
+        {
+            HappyStrings.Add(s);
+            return;
+        }
+
+        foreach (char c in HappyCharacters)
+        {
+            if (s.Length > 0 && c == s.Last()) continue;
+            BuildHappyString(s + c, n);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        int n = 1;
+        int k = 3;
+        string result = GetHappyString(n, k);
+        Console.WriteLine(result);
+    }
+}
